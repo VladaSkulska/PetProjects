cbc0fde68ba9ac0577b79799dc8a5ec5ba528bfa
Vlada Skulska <124265191+VladaSkulska@users.noreply.github.com>
2024-01-11 14:32:51 +0200

    Add initial project files via upload

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GuessNumberGame
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Game game = new();
+            game.StartGame();
+        }
+    }
+}
