d592ec46c91f2d3d3e56eb4c056e0f67a1edd4ae
Vlada Skulska <124265191+VladaSkulska@users.noreply.github.com>
2024-01-19 14:35:15 +0200

    Add interface for type handling

---
+++ GuessingGame/Interfaces/IGuessingTypeHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace GuessingGame.Interfaces
+{
+    public interface IGuessingTypeHandler
+    {
+        bool IsGuessOutOfRange(string guess, object lowerBound, object upperBound);
+        object GenerateRandomValue(object min, object max);
+    }
+}
