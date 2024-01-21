52b62676a27a8b4251111d0f9487c6f69c3512ee
Vlada Skulska <124265191+VladaSkulska@users.noreply.github.com>
2024-01-21 20:29:09 +0200

    Update CharTypeHandler.cs

--- GuessingGame/GameLogic/Handlers/CharTypeHandler.cs
+++ GuessingGame/GameLogic/Handlers/CharTypeHandler.cs
@@ -1,15 +1,28 @@
 namespace GuessingGame.GameLogic.Handlers
 {
-    public class CharTypeHandler : IGuessingTypeHandler
+    public class CharTypeHandler : IGuessingTypeHandler<char>
     {
-        public bool IsGuessOutOfRange(string guess, object lowerBound, object upperBound)
+        private char _lowerBound;
+        private char _upperBound;
+
+        public bool IsGuessOutOfRange(string guess, char lowerBound, char upperBound)
         {
-            return !(char.TryParse(guess, out char charGuess) && charGuess >= (char)lowerBound && charGuess <= (char)upperBound);
+            return !(char.TryParse(guess, out char charGuess) && charGuess >= lowerBound && charGuess <= upperBound);
         }
 
-        public object GenerateRandomValue(object min, object max)
+        public void GenerateRange()
         {
-            return GenerateRandomLetter((char)min, (char)max);
+            _lowerBound = GenerateRandomLetter('A', 'K');
+            _upperBound = GenerateRandomLetter('L', 'Z');
+        }
+        public (char LowerBound, char UpperBound) GetRange()
+        {
+            return (_lowerBound, _upperBound);
+        }
+
+        public char GenerateRandomValue(char min, char max)
+        {
+            return GenerateRandomLetter(min, max);
         }
 
         private char GenerateRandomLetter(char min, char max)
