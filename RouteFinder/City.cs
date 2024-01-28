b02fb28f50be9be25dae7e6ab8b76d65cae065c0
Скульська Влада <vlada.skulska@gmail.com>
2024-01-28 22:09:59 +0200

    Initial commit: Add RouteFinder solution

---
+++ RouteFinder/City.cs
@@ -0,0 +1,11 @@
+﻿public class City
+{
+    public string Name { get; set; }
+    public Dictionary<City, int> Distances { get; } = new Dictionary<City, int>();
+
+    public City(string name)
+    {
+        Name = name;
+    }
+}
+
